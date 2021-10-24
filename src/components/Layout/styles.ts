c047e7808810b64a1a543b667cece1f98b2369d6
RusakevichMR <rusakevichmr@gmail.com>
2021-10-24 12:54:56 +0300

    layout refactor

---
+++ src/components/Layout/styles.ts
@@ -0,0 +1,11 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles({
+  main: {
+    width: "100%",
+  },
+  link: {
+    color: "black",
+    textDecoration: "none",
+  },
+});
